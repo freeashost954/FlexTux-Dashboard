c39c3f661ad7e54b51389d37d9c61e81a9b56885
Geovane <87013843+GeovaneDev@users.noreply.github.com>
2023-09-24 15:29:26 +0000

    Primeiro commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # FlexTux-Dashboard
+
+Este repositório contém o código-fonte de uma dashboard simples para um bot do discord, com integração ao mongodb, desenvolvida usando JavaScript, HTML e CSS (Bootstrap).
+
+## Visão Geral
+
+A Dashboard do Bot é uma interface de usuário web que permite aos usuários interagirem com um bot de forma amigável e intuitiva. Ela fornece informações úteis, opções de configuração e recursos de controle para o bot.
+
+## Tecnologias Utilizadas
+
+As seguintes tecnologias foram usadas para desenvolver esta dashboard:
+
+- **JavaScript**: A linguagem de programação usada para criar funcionalidades interativas e lógica de controle.
+- **HTML**: A linguagem de marcação que define a estrutura e o conteúdo da página web.
+- **CSS (Bootstrap)**: O framework Bootstrap foi utilizado para estilizar a dashboard e torná-la responsiva.
+
+## Funcionalidades
+
+A Dashboard do Bot inclui as seguintes funcionalidades:
+
+- **Configuração**: Opções de configuração estão disponíveis para personalizar o comportamento do bot.
+- **Interface Amigável**: A interface foi projetada para ser fácil de usar, mesmo para usuários iniciantes.
+
+## Contribuição
+
+Se você deseja contribuir para este projeto, sinta-se à vontade para abrir issues, enviar pull requests ou fazer sugestões de melhorias. Toda contribuição é bem-vinda!
+
+## Licença
+
+Este projeto é licenciado sob a [Licença MIT](LICENSE), o que significa que você pode usá-lo livremente, modificar e distribuir conforme necessário.
+
+Esperamos que esta dashboard seja útil para gerenciar seu bot de forma eficaz e conveniente. Se tiver alguma dúvida ou problema, não hesite em entrar em contato conosco.
+
+Happy Coding!
---
+++ index.js
@@ -0,0 +1,269 @@
+const express = require('express');
+const passport = require('passport');
+const DiscordStrategy = require('passport-discord').Strategy;
+const session = require('express-session');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Discord = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const botToken = process.env.TOKEN;
+const client = new Discord.Client({
+    intents: [
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildMembers,
+        Discord.GatewayIntentBits.GuildModeration,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.MessageContent,
+    ],
+});
+client.login(botToken);
+
+const app = express();
+
+// Configuração do MongoDB
+mongoose.connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const User = mongoose.model('User', {
+    discordId: String,
+    username: String,
+    // Outros campos que você desejar
+});
+
+const Server = mongoose.model('Server', {
+    server_id: String,
+    custom_prefix: String,
+});
+
+// Configuração do Passport
+passport.use(new DiscordStrategy({
+    clientID: process.env.CLIENTID,
+    clientSecret: process.env.CLIENTSECRET,
+    callbackURL: process.env.CALLBACKURL,
+    scope: ['identify', 'guilds'],
+}, (accessToken, refreshToken, profile, done) => {
+    // Aqui, você pode salvar o perfil do usuário no MongoDB ou realizar outras ações
+    return done(null, profile);
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+});
+
+// Configuração da sessão
+app.use(session({
+    secret: process.env.SESSIONSECRET,
+    resave: true,
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Adicione o bodyParser para lidar com solicitações POST
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Rota de login
+app.get('/auth/discord', passport.authenticate('discord'));
+
+// Rota de callback após o login
+app.get('/auth/discord/callback', passport.authenticate('discord', {
+    failureRedirect: '/login-failed',
+}), async (req, res) => {
+    // Salvar ou atualizar o usuário no MongoDB
+    const user = await User.findOne({ discordId: req.user.id });
+    if (!user) {
+        const newUser = new User({
+            discordId: req.user.id,
+            username: req.user.username,
+        });
+        await newUser.save();
+    }
+
+    res.redirect('/dashboard');
+});
+
+// Função para renderizar a headerbar
+function renderHeaderbar(user) {
+    return `
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/dashboard">Dashboard (Fase Alfa)</a>
+                <div class="collapse navbar-collapse justify-content-end">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <img src="https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png" alt="User Avatar" class="user-avatar">
+                        </li>
+                        <li class="nav-item">
+                            <span class="navbar-text">${user.username}</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    `;
+}
+
+// Rota protegida - Dashboard
+app.get('/dashboard', async (req, res) => {
+    // Apenas usuários autenticados têm acesso a esta rota
+    if (req.isAuthenticated()) {
+        const user = req.user;
+
+        // Usar a biblioteca Discord.js para obter os servidores (guilds) do usuário
+        const botAdminGuilds = await getBotAdminGuilds(user);
+
+        const headerbar = renderHeaderbar(user);
+
+        res.send(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Dashboard</title>
+                <!-- Adicione os links CSS do Bootstrap aqui -->
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+                <link rel="stylesheet" type="text/css" href="style.css">
+            </head>
+            <body class="dark"> <!-- Aplicando a classe "dark" ao corpo da página -->
+                ${headerbar} <!-- Headerbar -->
+                <!-- Conteúdo da página -->
+                <div class="container mt-4">
+                    <h1 class="section-title">Painel do Usuário</h1>
+                    <p>Escolha um servidor para alterar o prefixo:</p>
+                    <form action="/choose-server" method="post">
+                        <div class="form-group">
+                            <select class="form-control" name="server">
+                                ${botAdminGuilds.map(server => `<option value="${server.id}">${server.name}</option>`).join('')}
+                            </select>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Escolher</button>
+                    </form>
+                </div>
+                <!-- Adicione os scripts do Bootstrap aqui (opcional) -->
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+            </body>
+            </html>
+        `);
+    } else {
+        res.redirect('/auth/discord');
+    }
+});
+
+// Função para obter os servidores (guilds) onde o bot tem permissão de administrador
+async function getBotAdminGuilds(user) {
+    const botAdminGuilds = [];
+
+    for (const guild of user.guilds) {
+        const guildObject = client.guilds.cache.get(guild.id);
+
+        if (guildObject && guildObject.members.cache.has(client.user.id) && guildObject.members.cache.get(client.user.id).permissions.has(Discord.PermissionFlagsBits.ManageGuild)) {
+            botAdminGuilds.push({
+                id: guild.id,
+                name: guildObject.name
+            });
+        }
+    }
+
+    return botAdminGuilds;
+}
+
+// Rota para processar a escolha do servidor
+app.post('/choose-server', (req, res) => {
+    // Apenas usuários autenticados têm acesso a esta rota
+    if (req.isAuthenticated()) {
+        const selectedServerId = req.body.server;
+        res.redirect(`/change-prefix?server=${selectedServerId}`);
+    } else {
+        res.redirect('/auth/discord');
+    }
+});
+
+// Rota para alterar o prefixo
+app.get('/change-prefix', async (req, res) => {
+    // Apenas usuários autenticados têm acesso a esta rota
+    if (req.isAuthenticated()) {
+        const user = req.user;
+        const serverId = req.query.server;
+        const successMessage = req.query.success === 'true' ? 'Prefixo alterado com sucesso!' : '';
+        if (!serverId){
+            res.redirect('/dashboard');
+        }
+
+        // Recupere o prefixo atual do servidor a partir do banco de dados
+        let serverData = await Server.findOne({ server_id: serverId });
+
+        // Se o servidor não existir no banco de dados, crie um com o prefixo padrão "$"
+        if (!serverData) {
+            serverData = new Server({
+                server_id: serverId,
+                custom_prefix: '!',
+            });
+            await serverData.save();
+        }
+
+        const headerbar = renderHeaderbar(user);
+
+        res.send(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Alterar Prefixo</title>
+                <!-- Adicione os links CSS do Bootstrap aqui -->
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+                <link rel="stylesheet" type="text/css" href="style.css">
+            </head>
+            <body class="dark"> <!-- Aplicando a classe "dark" ao corpo da página -->
+                ${headerbar} <!-- Headerbar -->
+                <!-- Conteúdo da página -->
+                <div class="container mt-4">
+                    <h1 class="section-title">Alterar Prefixo</h1>
+                    <p>Username do Usuário: ${user.username}</p>
+                    <p>Servidor Selecionado: ${serverData.server_id}</p>
+                    <p>${successMessage}</p>
+                    <form action="/save-prefix" method="post">
+                        <input type="hidden" name="serverId" value="${serverId}">
+                        <div class="form-group">
+                            <label for="prefix">Novo Prefixo:</label>
+                            <input type="text" id="prefix" name="prefix" value="${serverData.custom_prefix}" class="form-control" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Alterar Prefixo</button>
+                    </form>
+                </div>
+                <!-- Adicione os scripts do Bootstrap aqui (opcional) -->
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+            </body>
+            </html>
+        `);
+    } else {
+        res.redirect('/auth/discord');
+    }
+});
+
+// Rota para salvar a alteração do prefixo
+app.post('/save-prefix', async (req, res) => {
+    // Apenas usuários autenticados têm acesso a esta rota
+    if (req.isAuthenticated()) {
+        const newPrefix = req.body.prefix;
+        const serverId = req.body.serverId;
+
+        // Atualize o prefixo no banco de dados
+        await Server.updateOne({ server_id: serverId }, { $set: { custom_prefix: newPrefix } }, { upsert: true });
+
+        // Redirecione o usuário de volta para /change-prefix com uma mensagem de sucesso
+        res.redirect('/change-prefix?server=' + serverId + '&success=true');
+    } else {
+        res.redirect('/auth/discord');
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
